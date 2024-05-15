989bb38756b2852126441309a6ea997af8b6e7f3
nisol <nisolmedinaue@gmail.com>
2024-05-15 14:52:15 +0200

    deleteBUTTON-issues

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import styled from "styled-components";
+
 
 function Navbar() {
     return (
 <>
-<NavContainer>
+
+    
     <h2>BodyDesigners<span>Responsive</span></h2>
     <div>
         <a href="/">Home</a>
-        <a href="/WorkoutPage">Workout</a>
-        <a href="/FormPage">Create workout</a>
-        <a href="/AboutmePage">About Me</a>
+        <a href="/">Workout</a>
+        <a href="/">Create workout</a>
+        <a href="/">About Me</a>
     </div>
-</NavContainer>
 
 </>
  
@@ -20,20 +20,3 @@ function Navbar() {
 }
 export default Navbar;
 
-const NavContainer = styled.nav`
-h2{
-    font-weight: 400:
-    span{
-        font-wight: bold;
-    }
-}
-paddiing: 4rem:
-background-color: #333;
-display: flex;
-aling-items: center;
-justify-content: space-between;
-a{
-    color: whithe;
-    margin-right:1rem;
-}
-`
--- src/pages/WorkoutPage.jsx
+++ src/pages/WorkoutPage.jsx
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
-const WorkoutPage=({workouts})=>{
+
+function WorkoutPage({workouts, setWorkouts}) {
+
+    /*const deleteWorkout= workoutId=>{
+        const filteredWorkout= workouts.filter(workout=> workout.id !== workoutId);
+        console.log(workoutId);
+        return setWorkouts(filteredWorkout)
+
+    }; */
+    
     return(
-     <>
+     <div className="bigCard">
+
      <h1>Our Workouts</h1>
-     {workouts.map((workout)=>{
+
+     {workouts.map((workout)=> {
         return(
             <div key={workout.id}className="card">
 
@@ -12,11 +23,14 @@ const WorkoutPage=({workouts})=>{
                 <h2>{workout.name}</h2>
                 <p>Instruction:{workout.instruction}</p>
 
+              { /* <button className="btnDelete" onClick={() => deleteWorkout(workout.id)}>Delete</button>*/}
+
                 <Link to="/"><button className="Btn" >Back Home</button></Link>
+
             </div>
         );
      })}
-     </>
+     </div>
     );
 };
 
